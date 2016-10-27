2b23f270d5869ac14095cbad65e8b9919e01761a
lntoTheRain <lntotherain@qq.com>
2016-10-27 15:49:44 +0800

    initialize project

---
+++ yezizhibo/yezizhibo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  yezizhibo
+//
+//  Created by mac on 2016/10/27.
+//  Copyright © 2016年 julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+    }
+
+
+}
+
